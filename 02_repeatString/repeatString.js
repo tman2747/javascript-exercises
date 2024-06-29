a520e47d9c6789ba6c3b3424247b6982704ea0f7
Triston Land <tristonland@gmail.com>
2024-06-29 09:43:08 -0600

    finish 01 & 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let finalstring = ''
+
+    if (num < 0) {
+        return "ERROR"
+    }
+    for (let i = 0; i < num; i++) {
+        finalstring += string
+    }
+    return finalstring
 
 };
 

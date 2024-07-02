1a4d76de2f60308229d84d46c285499449ce0f93
Triston Land <tristonland@gmail.com>
2024-07-01 22:19:29 -0600

    added to sumall not complete yet-needs error checking

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    let sumAll = 0;
+    for (let i = start; i <= end; i++)
+    {
+        sumAll += i;
+    }
+    return sumAll
 };
-
+sumAll(1,4)
 // Do not edit below this line
 module.exports = sumAll;

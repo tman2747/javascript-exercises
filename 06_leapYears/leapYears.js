2d8827fa09587542e7d88f519e87301102da00b9
Triston Land <tristonland@gmail.com>
2024-07-06 13:08:31 -0600

    finish 06 leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+/**
+ * @param {number} year
+ */
+const leapYears = function(year) 
+{
+    if (year % 4 == 0 || year % 400 == 0)
+    {
+        if (year % 100 == 0 && year % 400 != 0)
+        {
+            return false
+        }
+        return true
+        
+    }
+    return false
+    
 };
-
+console.log(leapYears(1985))
 // Do not edit below this line
 module.exports = leapYears;

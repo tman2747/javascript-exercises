28b025858aeb1b146ae69af47b457e0b7b2f28d0
Triston Land <tristonland@gmail.com>
2024-07-06 13:24:21 -0600

    Finish 07 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const convertToCelsius = function() {
+/**
+ * 
+ * @param {number} temp 
+ */
+const convertToCelsius = function(temp)
+{
+return Math.round(((temp - 32.0) * 5/9)*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp)
+{
+  return Math.round(((temp * 9/5) + 32)*10)/10
 };
 
+console.log(convertToFahrenheit(21))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,

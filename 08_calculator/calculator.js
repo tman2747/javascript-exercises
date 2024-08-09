585041d94f1aab345914415a23a51275d6288bf0
Triston Land <tristonland@gmail.com>
2024-08-09 00:50:12 -0600

    finish 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((accumulator,currentIndex)=>{
+    return accumulator += currentIndex
+  },0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((accumulator,currentIndex)=>{
+    return accumulator *= currentIndex
+  },1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a == 1 || a == 0 )
+    return 1;
+  return a * factorial(a-1)
 };
-
+console.log(factorial(5))
 // Do not edit below this line
 module.exports = {
   add,

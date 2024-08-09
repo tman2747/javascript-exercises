1a1386db84abeb221a838dee79332d7612281b80
Triston Land <tristonland@gmail.com>
2024-08-09 01:38:33 -0600

    finish 09 Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string)
+{
+    let stringArray = []
+    for (let i = 0; i < string.length; i++)
+    {
+        if (/^[a-z0-9]+$/i.test(string[i]))
+        {
+            stringArray.push(string[i].toLowerCase());
+        }
 
+    }
+    const testArray = JSON.parse(JSON.stringify(stringArray)).reverse();
+    for (let i = 0; i < stringArray.length; i++)
+    {
+        if(stringArray[i] == testArray[i])
+        {
+            console.log(`${stringArray[i]} == ${testArray[i]}`)
+            continue;
+        }
+        return false
+    }
+
+    return true
 };
 
+console.log(palindromes('A car, a man, a maraca.'))
 // Do not edit below this line
 module.exports = palindromes;

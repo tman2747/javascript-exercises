b940216e889fc634b4ecf628b4a8c7298c35672f
Triston Land <tristonland@gmail.com>
2024-08-11 12:52:59 -0600

    finish 11 get titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+const getTheTitles = function(books) {
+    let titles = books.map((book)=>
+    {
+        return book.title
+    })
+    return titles
 };
+
+console.log(getTheTitles(books))
 
 // Do not edit below this line
 module.exports = getTheTitles;
